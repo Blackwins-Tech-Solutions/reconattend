a3c6041cf0c57d72a426843f136022c45da77000
Muthulakshmi <51967550+MuthulakshmiShanmugam@users.noreply.github.com>
2023-08-07 13:24:45 +0100

    base code for reconattendplus with docker and jenkins in aws ec2

--- attendance/server.js
+++ attendance/server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // for testing index page 
 app.get("/", (req, res) => {
-    res.send(`<h1>Welcome To our ReconAttendPlus App Attendance Module holass madiras!</h1>`)
+    res.send(`<h1>Welcome To our ReconAttendPlus App Attendance Module holass madira!</h1>`)
 });
 //app.use(router)
 
